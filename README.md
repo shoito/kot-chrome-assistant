f9b70a1ff9388954b8fd94ff32e26acfa5c6a250
shoito <shoito@users.noreply.github.com>
2019-02-07 20:37:34 +0900

    v0.0.1

---
+++ README.md
@@ -0,0 +1,6 @@
+# MyレコーダーChromeアシスタント
+
+[WIP]
+
+Chromeウェブストア - MyレコーダーChromeアシスタント・ページ
+https://chrome.google.com/webstore/detail/pifbdpooppfkllaiobkaoeocbfmpabaj/
---
+++ src/inject/inject.js
@@ -0,0 +1,4 @@
+(function() {
+  let now=new Date(),today=[now.getFullYear(),now.getMonth()+1,now.getDate()].map(d=>d.toString().padStart(2,'0')).join(''),today_history_item=localStorage.getItem('PARSONAL_BROWSER_RECORDER@RECORD_HISTORY_'+JSON.parse(localStorage.getItem('PARSONAL_BROWSER_RECORDER@SETTING')).user.user_token);today_histories=JSON.parse(today_history_item?today_history_item:'[]').filter(i=>i.send_timestamp.startsWith(today)),isClockedIn=today_histories.some(h=>h.name==='出勤'),isClockedOut=today_histories.some(h=>h.name==='退勤');isClockedIn?document.querySelector('.record-clock-in').style.opacity=0.3:isClockedOut?document.querySelector('.record-clock-out').style.opacity=0.3:null;
+  console.log('Content Scripts is injected by KoT Chrome Assistant.');
+})();
