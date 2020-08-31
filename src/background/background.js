c110daf98e5736b89870ffe849d1e7c4a004b329
shoito <shoito@users.noreply.github.com>
2020-08-31 21:12:26 +0900

    fix: cors for content scripts

--- src/background/background.js
+++ src/background/background.js
@@ -1,3 +1,28 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  sendResponse(true);
+  if (msg.contentScriptQuery === 'postMessage'
+      || msg.contentScriptQuery === 'changeStatus') {
+    fetch(msg.endpoint, {
+      'method': 'POST',
+      'headers': msg.headers,
+      'body': msg.body
+    })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res && res.ok) {
+        sendResponse({ 'status': 'success' });
+      } else {
+        console.error(JSON.stringify(res));
+        sendResponse({ 'status': 'failed' });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      sendResponse({ 'status': 'failed' });
+    });
+
+    return true;
+  }
+
+  sendResponse({ 'status': 'listener is missing.\n' + msg });
+  return true;
 });
