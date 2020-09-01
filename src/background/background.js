ce94403a681acfaf6580a859bd3a8074b1ca8ea7
shoito <shoito@users.noreply.github.com>
2020-09-01 09:03:35 +0900

    refact: validate endpoint

--- src/background/background.js
+++ src/background/background.js
@@ -1,25 +1,46 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg && (msg.contentScriptQuery === 'postMessage'
-      || msg.contentScriptQuery === 'changeStatus')) {
-    fetch(msg.endpoint, {
-      'method': 'POST',
-      'headers': msg.headers,
-      'body': msg.body
-    })
-    .then((res) => res.json())
-    .then((res) => {
-      if (res && res.ok) {
-        sendResponse({ 'status': 'success' });
-      } else {
-        console.error(JSON.stringify(res));
-        sendResponse({ 'status': 'failed' });
-      }
-    })
-    .catch((err) => {
-      console.error(err);
+const postRequest = (endpoint, headers, body, sendResponse) => {
+  fetch(endpoint, {
+    'method': 'POST',
+    'headers': headers,
+    'body': body
+  })
+  .then((res) => res.json())
+  .then((res) => {
+    if (res && res.ok) {
+      sendResponse({ 'status': 'success' });
+    } else {
+      console.error(JSON.stringify(res));
       sendResponse({ 'status': 'failed' });
-    });
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    sendResponse({ 'status': 'failed' });
+  });
+};
 
+const validateEndpoint = (endpoint) => {
+  const whitelist = [
+    'https://slack.com/api/chat.postMessage',
+    'https://slack.com/api/users.profile.set',
+    'https://hooks.slack.com/services/',
+  ]
+  return whitelist.some((l) => endpoint.startsWith(l));
+};
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (!msg) {
+    sendResponse({ 'status': 'listener is missing.\n' + msg });
+    return true;
+  }
+
+  if (msg.contentScriptQuery === 'postMessage' && validateEndpoint(msg.endpoint)) {
+    postRequest(msg.endpoint, msg.headers, msg.body, sendResponse)
+    return true;
+  }
+
+  if (msg.contentScriptQuery === 'changeStatus' && validateEndpoint(msg.endpoint)) {
+    postRequest(msg.endpoint, msg.headers, msg.body, sendResponse)
     return true;
   }
 
